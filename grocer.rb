07b3e01fb5cdf1f983af7d58ce6fd78228a5f13e
Jake Bartlow <jtbartlow@gmail.com>
2019-03-31 20:10:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,14 @@
 def consolidate_cart(cart)
   count = 0 
+  i = nil
   cart.each do |item, attributes|
+    if item = i 
+      count += 1 
+      cart[item][attributes][:count] = count
+    end
+  end
+end
+    
 end
 
 def apply_coupons(cart, coupons)
