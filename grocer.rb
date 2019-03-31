166364824d460971633713c05539256996678683
Jake Bartlow <jtbartlow@gmail.com>
2019-03-31 20:13:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,23 +1,17 @@
 def consolidate_cart(cart)
-  count = 0 
-  i = nil
-  cart.each do |item, attributes|
-    if item = i 
-      count += 1 
-      [attributes][:count] = count
+  consolidated_cart = {}
+
+  cart.each do |item|
+    item_name = item.keys[0]
+
+    consolidated_cart[item_name] = item.values[0]
+
+    if consolidated_cart[item_name][:count] 
+      consolidated_cart[item_name][:count] += 1
+    else
+      consolidated_cart[item_name][:count] = 1
     end
   end
-    
-end
 
-def apply_coupons(cart, coupons)
-  # code here
-end
-
-def apply_clearance(cart)
-  # code here
-end
-
-def checkout(cart, coupons)
-  # code here
+  consolidated_cart
 end
