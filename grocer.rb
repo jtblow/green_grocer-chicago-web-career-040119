2fd4f2ccfe771817de5a820a09dc9fb9a78a1272
Jake Bartlow <jtbartlow@gmail.com>
2019-03-31 20:28:22 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -36,7 +36,7 @@ def apply_clearance(cart)
   cart.each do |item, attributes|
     if attributes[:clearance] == true 
       new_price = attributes[:price] * 0.80
-      attributes[:price] = new_price
+      attributes[:price] = new_price.round(2)
     end
   end
   cart
